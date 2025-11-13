f000aafa12010dc839eb3244c9232626cbcf9bdb
dorianBess <dorianbessiere2@gmail.com>
2025-11-13 14:02:27 +0100

    Add create_animals post

--- src/refuge_esn81/router/animals.py
+++ src/refuge_esn81/router/animals.py
@@ -1,6 +1,17 @@
 from fastapi import APIRouter, FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from refuge_esn81.database.database import get_db
+from refuge_esn81.schemas.animalSchema import AnimalCreate
+from src.refuge_esn81.services.animalService import AnimalService , Animal
 
 
 animalsRouter = APIRouter(prefix="/animals", tags=["animals"])
 
 # A compléter
+
+@animalsRouter.post('/')
+async def create_animals(animal : AnimalCreate , db: Session = Depends(get_db)):
+    service = AnimalService()
+    return service.create_animal(db,animal)
+
--- src/refuge_esn81/services/animalService.py
+++ src/refuge_esn81/services/animalService.py
@@ -4,7 +4,19 @@ from refuge_esn81.models.animal import Animal
 
 class AnimalService:
     def create_animal(self, db: Session, animal: AnimalCreate):
-        return db_animal
+        # Crée l'objet Animal SQLAlchemy
+        db_animal = Animal(
+            name=animal.name,
+            age=animal.age,
+            gender=animal.gender,
+            description=animal.description,
+            photo_url=animal.photo_url,
+            species_id=animal.species_id
+        )
+        db.add(db_animal)  # ajoute à la session
+        db.commit()  # commit dans la base
+        db.refresh(db_animal)  # récupère les champs générés (id, etc.)
+        return db_animal  # retourne l'objet créé
 
     def get_animals(self, db: Session, skip: int = 0, limit: int = 100):
         return db.query(Animal).offset(skip).limit(limit).all()
