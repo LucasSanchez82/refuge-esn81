565e8b505e34d8c4838a4671a7a30934e1a21813
Shrod18 <pauline.bertrand.461@gmail.com>
2025-11-13 15:24:20 +0100

    fix import animalschema qui cassait tout

--- src/refuge_esn81/router/animals.py
+++ src/refuge_esn81/router/animals.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, FastAPI, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from refuge_esn81.database.database import get_db
-from refuge_esn81.schemas.animalSchema import AnimalCreate as Animal
+from refuge_esn81.schemas.animalSchema import Animal, AnimalCreate
 from refuge_esn81.services.animalService import AnimalService
 
 animalsRouter = APIRouter(prefix="/api/animals", tags=["animals"])
@@ -12,7 +12,6 @@ async def get_animals(db: Session = Depends(get_db)):
     return service.get_animals(db)
 
 @animalsRouter.post('/', response_model=Animal)
-async def create_animals(animal : Animal , db: Session = Depends(get_db)):
+async def create_animals(animal: AnimalCreate, db: Session = Depends(get_db)):
     service = AnimalService()
-    return service.create_animal(db,animal)
-
+    return service.create_animal(db, animal)
